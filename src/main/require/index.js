87e69ca27403175948a370999e8af53436e701ae
Matthew Adams <matthew@matthewadams.me>
2019-06-04 07:55:59 -0500

    tighten up require

--- src/main/require/index.js
+++ src/main/require/index.js
@@ -19,7 +19,7 @@ const entriesIn = (dir, predicate = () => true) => {
 
 module.exports = {
   dirsIn: absoluteDir => entriesIn(absoluteDir, it => fs.statSync(it).isDirectory() && !it.endsWith('node_modules')),
-  jsFilesIn: absoluteDir => entriesIn(absoluteDir, it => /\.js$/.test(it)),
-  jsFilesExceptIndexIn: absoluteDir => entriesIn(absoluteDir, it => /\.js$/.test(it) && !/index\.js$/.test(it)),
-  jsonFilesIn: absoluteDir => entriesIn(absoluteDir, it => /\.json$/.test(it))
+  jsFilesIn: absoluteDir => entriesIn(absoluteDir, it => /\.js$/.test(it) && fs.statSync(it).isFile()),
+  jsFilesExceptIndexIn: absoluteDir => entriesIn(absoluteDir, it => /\.js$/.test(it) && fs.statSync(it).isFile() && !/index\.js$/.test(it)),
+  jsonFilesIn: absoluteDir => entriesIn(absoluteDir, it => /\.json$/.test(it) && fs.statSync(it).isFile())
 }
--- src/test/unit/require/require.spec.js
+++ src/test/unit/require/require.spec.js
@@ -9,11 +9,15 @@ const req = require('../../../main/require')
 
 describe('req', () => {
   it('should require all .js files', () => {
-    const result = req.jsFilesIn(`${__dirname}/dirWithJsFiles`)
+    let result = req.jsFilesIn(`${__dirname}/dirWithJsFiles`)
     expect(result.file0).to.equal(0)
     expect(result.file1).to.equal(1)
     expect(result.index.zero).to.equal(0)
     expect(result.index.one).to.equal(1)
+
+    result = req.jsFilesIn(`${__dirname}/dirWithDirsWithNamesThatLookLikeFiles`)
+    expect(result.dir0).not.to.be.ok()
+    expect(result.dir1).not.to.be.ok()
   })
 
   it('should require all .js files except index.js', () => {
@@ -30,8 +34,12 @@ describe('req', () => {
   })
 
   it('should require all .json files', () => {
-    const result = req.jsonFilesIn(`${__dirname}/dirWithJsonFiles`)
+    let result = req.jsonFilesIn(`${__dirname}/dirWithJsonFiles`)
     expect(result.file0).to.equal(0)
     expect(result.file1).to.equal(1)
+
+    result = req.jsonFilesIn(`${__dirname}/dirWithDirsWithNamesThatLookLikeFiles`)
+    expect(result.dir0).not.to.be.ok()
+    expect(result.dir1).not.to.be.ok()
   })
 })
