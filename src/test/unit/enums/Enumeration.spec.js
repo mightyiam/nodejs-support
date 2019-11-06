75bbbd67106110da42301935f7851dc2b32d5e23
Matthew Adams <matthew@matthewadams.me>
2019-11-06 09:41:46 +0100

    update enum test

--- src/test/unit/enums/Enumeration.spec.js
+++ src/test/unit/enums/Enumeration.spec.js
@@ -10,24 +10,37 @@ const ClassNotExtendableError = require('../../../main/errors/ClassNotExtendable
 
 describe('unit tests of Enumeration', function () {
   it('should create a new enum', () => {
-    const E = Enumeration.new({ name: 'BooleanValue', values: ['TRUE', 'FALSE'] })
-    expect(E.TRUE).to.be.ok()
-    expect(E.FALSE).to.be.ok()
-    expect(E.BOGUS).not.to.be.ok()
-    expect(E.isInstance(E.TRUE)).to.be.true()
-    expect(E.isClass(E)).to.be.true()
-    expect(E.of(E.TRUE)).to.equal(E.TRUE)
-    expect(E.of('TRUE')).to.equal(E.TRUE)
-    expect(E.of(0)).to.equal(E.TRUE)
-    expect(() => E.of('BOGUS')).to.throw(E.$ERROR$)
+    const Boolean = Enumeration.new({ name: 'BooleanValue', values: ['TRUE', 'FALSE'] })
+    const OneTwo = Enumeration.new({ name: 'OneTwo', values: ['ONE', 'TWO'] })
+
+    expect(Boolean.TRUE).to.be.ok()
+    expect(Boolean.FALSE).to.be.ok()
+    expect(Boolean.BOGUS).not.to.be.ok()
+    expect(Boolean.isEnumerationInstance(Boolean.TRUE)).to.be.true()
+    expect(Boolean.isEnumerationClass(Boolean)).to.be.true()
+    expect(Boolean.isInstance(Boolean.TRUE)).to.be.true()
+    expect(Boolean.isClass(Boolean)).to.be.true()
+    expect(Boolean.isEnumerationInstance(OneTwo.ONE)).to.be.true()
+    expect(Boolean.isEnumerationClass(OneTwo)).to.be.true()
+    expect(OneTwo.isInstance(OneTwo.ONE)).to.be.true()
+    expect(OneTwo.isClass(OneTwo)).to.be.true()
+    expect(OneTwo.isInstance(Boolean.TRUE)).to.be.false()
+    expect(OneTwo.isClass(Boolean)).to.be.false()
+    expect(Boolean.of(Boolean.TRUE)).to.equal(Boolean.TRUE)
+    expect(Boolean.of('TRUE')).to.equal(Boolean.TRUE)
+    expect(Boolean.of(0)).to.equal(Boolean.TRUE)
+    expect(() => Boolean.of('BOGUS')).to.throw(Boolean.$ERROR$)
       .that.has.property('code').that.equals('E_UNKNOWN_BOOLEAN_VALUE_ENUM_VALUE')
-    expect(Enumeration.isEnumerationInstance(E.TRUE)).to.be.true()
-    expect(Enumeration.isEnumerationClass(E)).to.be.true()
+    expect(Enumeration.isEnumerationInstance(Boolean.TRUE)).to.be.true()
+    expect(Enumeration.isEnumerationClass(Boolean)).to.be.true()
   })
 
   it('should not allow extensions of enum classes', function () {
     const Super = Enumeration.new({ name: 'Super', values: ['Super'] })
-    class Sub extends Super {}
+
+    class Sub extends Super {
+    }
+
     expect(() => new Sub()).to.throw(ClassNotExtendableError)
   })
 })
